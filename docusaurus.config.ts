9915915003b72a6883a21eb7c03a5206cafecfd7
SumniWiki <sumni.wiki@gmail.com>
2026-09-23 11:51:15 +0900

    Update copyright notice in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -138,7 +138,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} SumniWiki, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} SumniWiki. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
