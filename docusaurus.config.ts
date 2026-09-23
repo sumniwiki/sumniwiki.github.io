9c0a3d1c260f49685244e163095e95bc9f3b99c7
SumniWiki <sumni.wiki@gmail.com>
2026-09-23 15:38:33 +0900

    Change favicon to sumniwiki_logo.ico

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'SumniWiki',
   tagline: 'Welcome to SumniWiki',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/sumniwiki_logo.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
