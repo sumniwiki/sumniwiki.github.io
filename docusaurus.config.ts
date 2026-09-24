e947deca694cb68bbdfbe53777ae9322d166b4a4
SumniWiki <sumni.wiki@gmail.com>
2026-09-24 12:21:10 +0900

    Update social card image in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,7 +79,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/sumniwiki-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
